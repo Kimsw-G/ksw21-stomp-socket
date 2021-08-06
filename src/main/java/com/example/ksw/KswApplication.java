93478622f1351a0ddf9ab648904aa3407052b631
Kimsw-G <kimsw3445@naver.com>
2021-08-06 12:06:50 +0900

    Initial commit

---
+++ src/main/java/com/example/ksw/KswApplication.java
@@ -0,0 +1,13 @@
+package com.example.ksw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KswApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KswApplication.class, args);
+    }
+
+}
